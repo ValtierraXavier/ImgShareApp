16674f10795c90fbe75b2f06030d7f47bb28b37d
ValtierraXavier <xavier.valtierra@icloud.com>
2023-09-19 23:05:19 -0400

    fix mobile landscape css

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -9,20 +9,16 @@ export default function Nav({getAllPosts, openLoginModal, user, handleOpenAddPos
         {user ? 
             <div className = 'nav'>
                 <div className = 'navUserDetail'>
-                    <NavLink to = {`/me/${user.id}`}>
-                        <img height = "45rem" src={`${user.avatarImg}`} alt = 'user profile'></img>
+                    <NavLink to = {`/me/${user.id}`} >
+                        <img height = "40rem" src={`${user.avatarImg}`} alt = 'user profile'></img>
                     </NavLink>
                     <a className='userName' href = {`/me/${user.id}`}>
-                        <div >{user.username}</div>
+                        <div>{user.username}</div>
                     </a>
                 </div>
-                <div className='homeAndPost'>
                     <NavLink onClick={getAllPosts} className='homeButton' to = '/'>Home</NavLink>
-                    <div className='postButton' onClick={handleOpenAddPostModal}>Make a Post!</div>
-                </div>    
-                <div  className = 'signInButtons'>
+                    <button className='postButton' onClick={handleOpenAddPostModal}>Make a Post!</button>
                     <div onClick={handleSignout}  className = 'signOutButton'>Sign Out</div>
-                </div>
             </div>
             :
             <div className = 'nav'>
@@ -33,12 +29,8 @@ export default function Nav({getAllPosts, openLoginModal, user, handleOpenAddPos
                 <div className='homeAndPost'>
                     <NavLink className='homeButton' to = '/'>Home</NavLink> 
                 </div> 
-                    <div  className = 'signInButtons'>
-                        <div onClick ={openLoginModal} className = 'signInButton'>Sign In</div>
-                        <div className='signupButtonContainer'>
-                            <Link  to='/signup' className = 'signUpButton'>Sign Up</Link>
-                        </div>
-                    </div>        
+                    <div onClick ={openLoginModal} className = 'signInButton'>Sign In</div>
+                    <Link  to='/signup' className = 'signUpButton'>Sign Up</Link>
             </div>
         }
     </div>
